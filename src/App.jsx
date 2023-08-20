2b26aacbfc3f6b148b249b11597b18b2651d8747
[harish saifi] <harishsaifi2003@gmail.com>
2023-08-20 08:57:05 +0530

    complete boilerplate ofthe App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-const  App = ()=> {
-  
+import CustomerReviews from "./sections/CustomerReviews"
+import Footer from "./sections/Footer"
+import Hero from "./sections/Hero"
+import PopularProducts from "./sections/PopularProducts"
+import Services from "./sections/Services"
+import SpecialOffer from "./sections/SpecialOffer"
+import Subscribe from "./sections/Subscribe"
+import SuperQuality from "./sections/SuperQuality"
+
+const App = () => {
+
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+       <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts/>
+      </section>
+      <section className="padding">
+       <SuperQuality />
+      </section>
+      <section className="padding-x padding-y">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
+
+    </main>
   )
 }
 
