f8975ee9d2d9027f478c5cd9715a869d93024f41
[harish saifi] <harishsaifi2003@gmail.com>
2023-08-21 10:11:10 +0530

    hero section working

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Button = ({label,iconURL}) => {
   return (
     <button
-        className=""
+        className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red "
     >
        {label}
-       <img  src={iconURL}/>
+       <img  src={iconURL}
+        className="ml-2 rounded-full w-5 h-5"
+
+       />
     </button>
   ) 
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,33 +1,65 @@
 import React from 'react'
 import Button from '../components/button'
 import { arrowRight } from '../assets/icons'
+import {shoes, statistics } from '../constants'
+import { bigShoe1 } from '../assets/images'
+import Shoecard from '../components/Shoecard'
+
 
 const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container "
+      className="w-full flex xl:flex-row flex-col  justify-center min-h-screen gap-10 max-container "
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
-        <p> Our Summer collection</p>
-        <h1>
-          <span>
+      <div className="relative xl:w-2/5 flex flex-col  justify-center items-start w-full max-xl:padding-x pt-28 ">
+        <p className="text-xl font-montserrat text-coral-red"> Our Summer collection</p>
+        <h1 className="mt-10 font-palanquin  text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap realtive z-50 pr-10">
             The new Arrival
           </span>
           <br />
-          <span>
+          <span className="text-coral-red inline-block mt-3">
             Nike
           </span>
           Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm ">
           Discover stylish Nike arrivals,quality comfort , and innovation for your active life.
         </p>
         <Button 
         label="Show now" 
         iconURL={arrowRight}
+        
         /> 
+        <div className=" flex justify-start items-start w-full mt-20 gap-16 flex-wrap ">
+            {statistics.map((stat) =>(
+              <div>
+                <p className="text-4xl font-palanquin font-bold " >{stat.value}</p>
+                <p className="leading-7 font-montserrat text-slate-gray" >{stat.label}</p>
+              </div>
+            ))}
+        </div>
 
+      </div>
+      <div className=" relative flex-1 flex justify-center z-0  items-center xl:min-h-screen max-xl:py40 bg-hero bg-primary bg-cover bg-center  ">
+        <img 
+          src={bigShoe1}
+          alt='Shoes '
+          width={610}
+          height={500}
+          className=" object-contain relative z-10"
+        />
+        {/* <div>
+            {shoes.map((shoe)=>(
+              <div key={shoe}>
+                  <Shoecard 
+                    imgURL={shoe}
+                    changeBigShoeImage={()=>{}}
+                  />
+              </div>
+            ))}
+        </div> */}
       </div>
 
     </section>
