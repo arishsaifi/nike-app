bd88c1f9ec42ade2702d709f3b62eb5318ae7487
[harish saifi] <harishsaifi2003@gmail.com>
2023-08-21 08:54:50 +0530

    button component created

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = ({label,iconURL}) => {
+  return (
+    <button
+        className=""
+    >
+       {label}
+       <img  src={iconURL}/>
+    </button>
+  ) 
+}
+
+export default Button
